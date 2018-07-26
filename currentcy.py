6c183e08a76e1594bfb99960a015d48b740d8bd1
Danyl Fernandes <androidan1304@gmail.com>
2018-07-26 23:27:13 +0530

    Added FROM, TO, multiplier
    - Function is now generalised.
    - You can now use Currency Abbreviations to find the value.
    - With the multiplier parameter you can get how many 'FROM's you want to calculate

--- currentcy.py
+++ currentcy.py
@@ -9,6 +9,30 @@ def current_dollar_rate():
 	for text in soup.findAll('span', {'class': 'uccResultAmount'}):
 		return text.string
 
+def currentcy_rate(FROM, TO, multiplier = 1):
+	url_pOne = "https://www.xe.com/currencyconverter/convert/?Amount=1&From="
+	url_pTwo = str(FROM)
+	url_pThree = "&To="
+	url_pFour = str(TO)
+	rate = 1
+
+	url = url_pOne + url_pTwo + url_pThree + url_pFour
+	source_code = requests.get(url)
+	plain_text = source_code.text 
+	soup = BeautifulSoup(plain_text, "html.parser")
+	for text in soup.findAll('span', {'class': 'uccResultAmount'}):
+		rate = text.string
+	todays_rate = rate
+	total = float(multiplier) * float(todays_rate)
+
+	return round(total, 2)
+ 
+
+
+
+
+
+
 
 
 
